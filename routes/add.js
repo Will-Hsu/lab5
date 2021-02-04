5b6050dd4cdd6d2222623e4e9c2495f07dac641f
Will Hsu <whsu@ucsd.edu>
2021-02-03 22:40:01 -0800

    done

--- routes/add.js
+++ routes/add.js
@@ -4,7 +4,7 @@ exports.addFriend = function(request, response) {
 	var newfriend = {
 		"name": request.query.name,
 		"description": request.query.description,
-		"imageURL": "https://loremflickr.com/400/400/student, teen, girl"
+		"imageURL": "http://lorempixel.com/400/400/people"
 	}
 	data.friends.push(newfriend);
 	response.render('index', data);
